683b8419367b0862fff4badd31e0334676c4fc5c
jonas <jonas.voisard@gmail.com>
2024-09-23 23:19:02 +0200

    fix norm

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 22:35:04 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/09/23 23:12:17 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/09/23 23:18:39 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 t_size	ft_strlcpy(char *dst, const char *src, t_size len)
 {
-	const char *_src;
-	t_size _len;
+	const char	*_src;
+	t_size		_len;
 
 	_src = src;
 	_len = len;
@@ -25,5 +25,5 @@ t_size	ft_strlcpy(char *dst, const char *src, t_size len)
 	*dst = '\0';
 	while (*_src)
 		_src++;
-	return (_src - src);	
+	return (_src - src);
 }
