c0e47dc3fb881805319d373e74180ebd060c3be8
Jonas <jonas.voisard@gmail.com>
2024-10-01 12:43:36 +0200

    resor toupper

--- libft.h
+++ libft.h
@@ -17,7 +17,7 @@ typedef unsigned long	t_size;
 
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
-int		ft_isalnum(int c);
+int	ft_isalnum(int c);
 int		ft_isascii(int c);	
 int		ft_isprint(int c);
 t_size	ft_strlen(const char *str);
@@ -27,17 +27,17 @@ void	*ft_memset(void *str, int c, t_size n);
 void	*ft_memcpy(void *dst, const void *src, t_size n);
 void	*ft_memmove(void *dst, const void *src, t_size n);
 t_size	ft_strlcat(char *dst, const char *src, t_size dsize);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_memchr(const void *s, int c, t_size n);
-int		ft_memcmp(const void *s1, const void *s2, t_size n);
+int	ft_memcmp(const void *s1, const void *s2, t_size n);
 char	*ft_strnstr(const char *str, const char *find, t_size len);
-int		ft_atoi(const char *str);
+int	ft_atoi(const char *str);
 void	*ft_calloc(t_size count, t_size size);
 char	*ft_strdup(const char *str);
 char	*ft_substr(char const *s, unsigned int start, t_size len);
 char	*ft_strjoin(const char *a, const char *b);
 char	*ft_strtrim(char const *str, char const *set);
-#endif
+char	**ft_split(char const *s, char c);n#endif
