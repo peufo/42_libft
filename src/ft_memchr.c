53f08f7652ca1bc3cd487e803c62a4fbd4076b09
jonas <jonas.voisard@gmail.com>
2024-10-03 20:11:39 +0200

    fix memchr

--- src/ft_memchr.c
+++ src/ft_memchr.c
@@ -6,19 +6,21 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:05:56 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/09/27 23:36:47 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/03 20:10:18 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memchr(const void *s, int c, t_size n)
 {
 	const unsigned char	*p;
+	unsigned char _c;
 
 	p = s;
+	_c = (unsigned char)c;
 	if (n == 0)
 		return ((void *)0);
 	while (*p && --n)
-		if (*(p++) == c)
+		if (*(p++) == _c)
 			return ((void *)(p - 1));
 	if (*p == c)
 		return ((void *)(p));
