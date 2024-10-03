ce45ad711ddaa2dd0cb62da47da19bd59600e115
jonas <jonas.voisard@gmail.com>
2024-10-03 20:18:51 +0200

    fix: memcmp

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 11:11:34 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/09/27 23:36:54 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/03 20:18:12 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_memcmp(const void *s1, const void *s2, t_size n)
 		return (0);
 	a = (unsigned char *)s1;
 	b = (unsigned char *)s2;
-	while (--n && *a && *a == *b)
+	while (--n && *a == *b)
 	{
 		a++;
 		b++;
