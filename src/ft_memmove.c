fa835fd07e20384dc88f72d35214f7b08d53681f
jonas <jonas.voisard@gmail.com>
2024-10-03 20:01:34 +0200

    memmove ok

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 20:55:54 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/10/01 22:27:04 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/03 20:01:16 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ void	*ft_memmove(void *dst, const void *src, t_size n)
 	char	*_dst;
 	char	*_src;
 
+	if (!dst && !src)
+		return (0);
 	_src = (char *)src;
 	_dst = (char *)dst;
 	if (_dst < _src)
@@ -22,7 +24,7 @@ void	*ft_memmove(void *dst, const void *src, t_size n)
 		while (n--)
 			*(_dst++) = *(_src++);
 	}
-	else
+	else if (_dst > _src)
 	{
 		_src += n;
 		_dst += n;
