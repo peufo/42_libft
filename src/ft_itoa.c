9b91fcc7b106da4b12cbd21b79ec9eb4ecb1d362
jonas <jonas.voisard@gmail.com>
2024-10-05 00:16:22 +0200

    fix: itoa correct malloc

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 15:29:22 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/10/01 21:44:38 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/05 00:16:08 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 static char	*handle_positive(char *str, int n)
 {
-	while (n != 0)
+	while (n)
 	{
 		*(--str) = (n % 10) + '0';
 		n /= 10;
@@ -22,7 +22,7 @@ static char	*handle_positive(char *str, int n)
 
 static char	*handle_negative(char *str, int n)
 {
-	while (n != 0)
+	while (n)
 	{
 		*(--str) = -(n % 10) + '0';
 		n /= 10;
@@ -31,11 +31,26 @@ static char	*handle_negative(char *str, int n)
 	return (str);
 }
 
+static short	get_malloc_len(int n)
+{
+	short	len;
+
+	len = 1;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 
-	str = malloc(12);
+	str = malloc(get_malloc_len(n));
 	if (!str)
 		return (NULL);
 	str += 12;
