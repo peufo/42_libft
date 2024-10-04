9a3d0ff452c97c57c6ada9869aef1d6f0a0e0ca0
jonas <jonas.voisard@gmail.com>
2024-10-04 22:22:31 +0200

    fix: strnstr

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -6,35 +6,26 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:07:30 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/10/04 15:07:44 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/04 22:19:15 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static t_size	ft_startwith(const char *str, const char *start)
-{
-	while (*str && *start)
-		if (*(str++) != *(start++))
-			return (0);
-	if (*start)
-		return (0);
-	return (1);
-}
-
 char	*ft_strnstr(const char *str, const char *find, t_size len)
 {
-	t_size	find_len;
+	size_t	find_len;
 
 	if (!*find)
 		return ((char *)str);
-	find_len = ft_strlen(find);
-	if (len == find_len && str == find)
-		return ((char *)str);
-	if (len > find_len)
-	{
-		len -= find_len - 1;
-		while (len-- && *str)
-			if (ft_startwith(str++, find))
-				return ((char *)(str - 1));
-	}
-	return (0);
+	if (!len-- || !*str)
+		return (0);
+	find_len = ft_strlen(find) - 1;
+	while (*str++ != *find)
+		if (!len-- || !*str)
+			return (0);
+	while (ft_strncmp(str, find, find_len))
+		while (*str++ != *find)
+			if (!len-- || !*str)
+				return (0);
+	str--;
+	return ((char *)str);
 }
