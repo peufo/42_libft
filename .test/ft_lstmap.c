d34f78482ae36fcfda0eb78b9241139643ced876
Jonas <jonas.voisard@gmail.com>
2024-10-08 13:53:16 +0200

    set content to NULL

--- .test/ft_lstmap.c
+++ .test/ft_lstmap.c
@@ -27,10 +27,7 @@ int main()
 	lst->next->next = ft_lstnew(strdup("!"));
 	t_list *new_lst = ft_lstmap(lst, edit_content_toupper, del_content);
 
-	if (!strcmp(lst->content, "Hey")
-		|| !strcmp(lst->next->content, "mum")
-		|| !strcmp(lst->next->next->content, "!")
-	)
+	if (lst->content ||lst->next->content || lst->next->next->content)
 	{
 		printf("TEST FAILED %s:%d\n", __FILE__, __LINE__);
 		printf("List content is still defined");
