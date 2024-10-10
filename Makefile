4db4e618a3104ec59bf823f6d84f14e1d2ad6446
Jonas <jonas.voisard@gmail.com>
2024-10-10 11:32:01 +0200

    add better test options (-h -v -l -r)

--- Makefile
+++ Makefile
@@ -5,12 +5,12 @@ DIR_BUILD = build
 DIR_TEST = .test
 FLAGS = -Wall -Wextra -Werror
 
-SOURCES		=	ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c ft_lstdelone.c ft_lstiter.c ft_lstlast.c ft_lstmap.c ft_lstnew.c ft_lstsize.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memset.c ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_split.c ft_strchr.c ft_strdup.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlcpy.c ft_strlen.c ft_strmapi.c ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_strtrim.c ft_substr.c ft_tolower.c ft_toupper.c 
+SOURCES		=	ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c ft_lstdelone.c ft_lstiter.c ft_lstlast.c ft_lstmap.c ft_lstnew.c ft_lstsize.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memset.c ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_split.c ft_strchr.c ft_strdup.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlcpy.c ft_strlen.c ft_strmapi.c ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_strtrim.c ft_substr.c ft_tolower.c ft_toupper.c
 FUNCTIONS	=	$(subst .c,,$(notdir $(SOURCES)))
 OBJECTS		=	$(addsuffix .o, $(addprefix $(DIR_BUILD)/, $(FUNCTIONS)))
 TESTS		=	$(addprefix $(DIR_TEST)/$(DIR_BUILD)/, $(FUNCTIONS))
 
-SOURCES_BNS		=	ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c ft_lstdelone.c ft_lstiter.c ft_lstlast.c ft_lstmap.c ft_lstnew.c ft_lstsize.c 
+SOURCES_BNS		=	ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c ft_lstdelone.c ft_lstiter.c ft_lstlast.c ft_lstmap.c ft_lstnew.c ft_lstsize.c
 FUNCTIONS_BNS	=	$(subst .c,,$(notdir $(SOURCES_BNS)))
 OBJECTS_BNS		=	$(addsuffix .o, $(addprefix $(DIR_BUILD)/, $(FUNCTIONS_BNS)))
 TESTS_BNS		=	$(addprefix $(DIR_TEST)/$(DIR_BUILD)/, $(FUNCTIONS_BNS))
@@ -39,10 +39,10 @@ so: $(OBJECTS)
 	@cc -nostartfiles -shared -o libft.so $(OBJECTS)
 
 test: all $(TESTS) $(TESTS_BNS)
-	@./test.sh
+	@./test.zsh
 
 test-v: all $(TESTS) $(TESTS_BNS)
-	@./test.sh -v
+	@./test.zsh -v
 
 $(DIR_TEST)/$(DIR_BUILD)/%: $(DIR_TEST)/%.c $(DIR_BUILD)/%.o | $(DIR_TEST)/$(DIR_BUILD)
 	@cc $(FLAGS) -include libft.h -include $(DIR_TEST)/test.h $< -o $@ $(LIBS)
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 info() {
 	echo -e "\033[36m$1\033[0m"
@@ -16,28 +16,55 @@ overwrite_2() {
 	echo -e "\r\033[2A\033[0K$1"
 }
 
-test() {
-	info "\nMY TESTS\n"
-	VERBOSE=$([[ $@ =~ "-v" ]] && echo true || echo false)
+sed_i() {
+	if [[ $(uname) == "Linux" ]] ; then
+		sed -i "$@"
+	else
+		sed -i "" "$@"
+	fi
+}
 
-	TESTS_COUNT=0
-	TESTS_PASSED=0
+test() {
+	local usage=(
+		"test [-h|--help]			Print this help"
+		"test [-v|--verbose]			Print all test output"
+		"test [-l|--lib] own|unit|war|tester	Choose a specific test lib to use"
+		"test [-r|--reset]			Remove and re-download external libs"
+	)
+	local lib="all"
+	local is_verbose=false
+	local is_reset=false
+
+	opterr() { echo >&2 "test: Unknown option '$1'" }
+
+	while (( $# )); do
+		case $1 in
+		-h|--help)			printf "%s\n" $usage && return	;;
+		-v|--verbose)		is_verbose=true					;;
+		-r|--reset)			is_reset=true					;;
+		-l|--lib)			shift; lib=$1					;;
+		-l=*|--lib=*)		lib="${1#*=}"					;;
+		-*)					opterr $1 && return 2			;;
+		*)					positional+=("${@[@]}"); break	;;
+		esac
+		shift
+	done
 
 	run_test() {
 		PATH_NAME=$1
-		VERBOSE=$2
+		is_verbose=$2
 		RESULT=$($PATH_NAME)
 		TEST_NAME=${PATH_NAME#*.test/build/}
 		((TESTS_COUNT++))
 		if [[ $RESULT == "" ]] ; then
 			((TESTS_PASSED++))
-			if $VERBOSE ; then
+			if $is_verbose ; then
 				success "$TEST_NAME\tOK"
 			else
 				overwrite "$(success "$TEST_NAME\tOK")"
 			fi
 		else
-			if ! $VERBOSE ; then overwrite
+			if ! $is_verbose ; then overwrite
 			fi
 			warning "$TEST_NAME\tFAIL"
 			echo -e "./$TEST_NAME.c ./.test/$TEST_NAME.c\n"
@@ -47,62 +74,90 @@ test() {
 
 	TESTS_BUILD_DIR=".test/build"
 	if [ ! -d  "$TESTS_BUILD_DIR" ] ; then
-		warning "Tests are not compiled. Please run \"make test\""
+		info "Run \"make test\""
+		make test
 		return
 	fi
 
-	if ! $VERBOSE ; then echo ""
-	fi
-	for P in $TESTS_BUILD_DIR/*
-	do
-		run_test "$P" "$VERBOSE"
-	done
-	if ! $VERBOSE ; then overwrite
+	# MY OWN TESTS
+	if [[ $lib == "all" || $lib == "own" ]] ; then
+		info "\nMY OWN TESTS\n"
+		local TESTS_COUNT=0
+		local TESTS_PASSED=0
+		if ! $is_verbose ; then echo "" ; fi
+		for P in $TESTS_BUILD_DIR/*
+		do
+			run_test "$P" "$is_verbose"
+		done
+		if ! $is_verbose ; then overwrite ; fi
+		if [[ $TESTS_PASSED == $TESTS_COUNT ]]; then
+			success "All tests [$TESTS_PASSED/$TESTS_COUNT] passed 🎉🎉🎉"
+		else
+			success "[$TESTS_PASSED/$TESTS_COUNT] tests passed"
+			warning "But $(($TESTS_COUNT - $TESTS_PASSED)) tests failed"
+		fi
 	fi
 
-	if [[ $TESTS_PASSED == $TESTS_COUNT ]]; then
-		success "All tests [$TESTS_PASSED/$TESTS_COUNT] passed 🎉🎉🎉"
-	else
-		success "[$TESTS_PASSED/$TESTS_COUNT] tests passed"
-		warning "But $(($TESTS_COUNT - $TESTS_PASSED)) tests failed"
-	fi
-
-	# ENSURE TEST EXTERNE DIR existe
+	# ENSURE TEST EXTERNE DIR STATE
 	EXT_TEST_DIR=".test/extern"
+	if $is_reset ; then
+		rm -rf "$EXT_TEST_DIR"
+		return
+	fi
 	if [ ! -d "$EXT_TEST_DIR" ] ; then
 		mkdir "$EXT_TEST_DIR"
 	fi
 
 	# LIB-UNIT-TESTS
-	EXT_TEST_REPO="https://github.com/alelievr/libft-unit-test"
-	EXT_TEST="$EXT_TEST_DIR/libft-unit-tests"
+	if [[ $lib == "all" || $lib == "unit" ]] ; then
+		EXT_TEST_REPO="https://github.com/alelievr/libft-unit-test"
+		EXT_TEST="$EXT_TEST_DIR/libft-unit-tests"
 
-	sed -i "s;LIBFTDIR	=	../libft;LIBFTDIR	=	$(pwd);" "$EXT_TEST/Makefile"
+		sed_i "s;LIBFTDIR	=	../libft;LIBFTDIR	=	$(pwd);" "$EXT_TEST/Makefile"
 
-	info "\nUNITS TEST LIB ($EXT_TEST_REPO)\n"
-	if [ ! -d "$EXT_TEST" ] ; then
-		git clone "$EXT_TEST_REPO" "$EXT_TEST"
-	fi
-	EXT_TEST_OUTPUT=$(make -C "$EXT_TEST" f | tr -d '\0')
-	EXT_TEST_FAILS=$(cat "$EXT_TEST/result.log" | sed -E '/.+:\ *(\[OK\]\ |\{protected\})+/d')
-	if $VERBOSE ; then
-		echo "$EXT_TEST_OUTPUT"
-	elif [[ $EXT_TEST_FAILS == "" ]] ; then
-		success "NO FAILS DETECTED 🎉🎉🎉"
-	else
-		warning "$EXT_TEST_FAILS"
+		info "\nUNITS TEST LIB ($EXT_TEST_REPO)\n"
+		if [ ! -d "$EXT_TEST" ] ; then
+			git clone --depth=1 "$EXT_TEST_REPO" "$EXT_TEST"
+			rm -rf "$EXT_TEST/.git"
+		fi
+		EXT_TEST_OUTPUT=$(make -C "$EXT_TEST" f | tr -d '\0')
+		EXT_TEST_FAILS=$(cat "$EXT_TEST/result.log" | sed -E '/.+:\ *(\[OK\]\ |\{protected\})+/d')
+		if $is_verbose ; then
+			echo "$EXT_TEST_OUTPUT"
+		elif [[ $EXT_TEST_FAILS == "" ]] ; then
+			success "NO FAILS DETECTED 🎉🎉🎉"
+		else
+			warning "$EXT_TEST_FAILS"
+		fi
 	fi
 
 	# WAR-MACHINE
-	EXT_TEST_REPO="https://github.com/y3ll0w42/libft-war-machine"
-	EXT_TEST="$EXT_TEST_DIR/libft-war-machine"
-	info "\nLIBFT WAR MACHINE ($EXT_TEST_REPO)\n"
-	if [ ! -d "$EXT_TEST" ] ; then
-		git clone "$EXT_TEST_REPO" "$EXT_TEST"
-		echo "$WAR_MACHINE_CONFIG" > "$EXT_TEST/my_config.sh"
+	if [[ $lib == "all" || $lib == "war" ]] ; then
+		EXT_TEST_REPO="https://github.com/y3ll0w42/libft-war-machine"
+		EXT_TEST="$EXT_TEST_DIR/libft-war-machine"
+		info "\nLIBFT WAR MACHINE ($EXT_TEST_REPO)\n"
+		if [ ! -d "$EXT_TEST" ] ; then
+			git clone --depth=1 "$EXT_TEST_REPO" "$EXT_TEST"
+			rm -rf "$EXT_TEST/.git"
+			echo "$WAR_MACHINE_CONFIG" > "$EXT_TEST/my_config.sh"
+		fi
+		sed_i "/clear/d" "$EXT_TEST/grademe.sh"
+		"$EXT_TEST/grademe.sh" -s -u
 	fi
-	sed -i "/clear/d" "$EXT_TEST/grademe.sh"
-	"$EXT_TEST/grademe.sh" -s -u
+
+	# LIBFT-TESTER
+	if [[ $lib == "all" || $lib == "tester" ]] ; then
+		EXT_TEST_REPO="https://github.com/Tripouille/libftTester"
+		EXT_TEST="$EXT_TEST_DIR/libftTester"
+		info "\nLIBFT-TESTER ($EXT_TEST_REPO)\n"
+		if [ ! -d "$EXT_TEST" ] ; then
+			git clone --depth=1 "$EXT_TEST_REPO" "$EXT_TEST"
+			rm -rf "$EXT_TEST/.git"
+			echo "$WAR_MACHINE_CONFIG" > "$EXT_TEST/my_config.sh"
+		fi
+		#sed_i "/clear/d" "$EXT_TEST/grademe.sh"
+		#"$EXT_TEST/grademe.sh" -s -u
+	fi
 }
 
 WAR_MACHINE_CONFIG="
