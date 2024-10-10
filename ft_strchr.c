50f9299e7c242f56a6bcf58b4d24c9409e2a9072
Jonas <jonas.voisard@gmail.com>
2024-10-10 18:26:38 +0200

    fix cast char in strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -15,9 +15,9 @@
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
-		if (*s++ == c)
+		if (*s++ == (unsigned char)c)
 			return ((char *)(s - 1));
-	if (*s == c)
+	if (*s == (unsigned char)c)
 		return ((char *)s);
 	return ((void *)0);
 }
