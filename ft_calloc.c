6b96b88578257453272fd3a61778d2ff9209f040
Jonas <jonas.voisard@gmail.com>
2024-10-11 10:54:00 +0200

    fix: calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 19:30:04 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/10/08 16:31:09 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/11 10:52:53 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 	size_t	len;
 
-	if ((long)count < 0 && (long)size < 0)
-		return (0);
-	if (count == 0 || size == 0)
-		len = 1;
-	else
-		len = count * size;
+	len = count * size;
 	ptr = malloc(len);
 	if (ptr)
 		ft_bzero(ptr, len);
