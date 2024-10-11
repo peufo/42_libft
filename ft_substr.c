294f95c1d10f0bf40e021293570f70887a742418
Jonas <jonas.voisard@gmail.com>
2024-10-11 10:48:11 +0200

    fix: substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,22 @@
 /*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 20:20:15 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/10/08 16:34:00 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/11 10:44:51 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*ft_empty_str(void)
+{
+	char	*str;
+
+	str = malloc(1);
+	if (!str)
+		return (0);
+	*str = '\0';
+	return (str);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -20,10 +31,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (0);
 	str_len = ft_strlen(s);
-	if (!len || str_len < (size_t)start)
-		len = 0;
-	else if (str_len < len)
-		len = str_len;
+	if (start >= str_len)
+		return (ft_empty_str());
+	if (len > (str_len - start))
+		len = str_len - start;
 	sub = ft_calloc(len + 1, sizeof(char));
 	if (!sub)
 		return (0);
